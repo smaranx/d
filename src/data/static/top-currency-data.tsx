db84292c3399b624ea829f5864b1a411bea14cc4
Smaran Ummadishetty <speak2smaran@gmail.com>
2022-09-08 12:55:45 +0530

    ic

---
+++ src/data/static/top-currency-data.tsx
@@ -0,0 +1,75 @@
+import { Bitcoin } from "@/components/icons/bitcoin";
+import { Ethereum } from "@/components/icons/ethereum";
+import { Tether } from "@/components/icons/tether";
+import { Bnb } from "@/components/icons/bnb";
+import { Usdc } from "@/components/icons/usdc";
+import { Cardano } from "@/components/icons/cardano";
+import { Doge } from "@/components/icons/doge";
+
+export const TopCurrencyData = [
+  {
+    id: 1,
+    coin: {
+      name: "0xde0f...2c661a",
+      symbol: "",
+    },
+
+    quantity: "$600,000",
+    percentage: 20.797468354,
+  },
+  {
+    id: 2,
+    coin: { symbol: "", name: "0x9598...c0a59b" },
+    quantity: "$260,000",
+    percentage: 12.131912059,
+  },
+  {
+    id: 3,
+    coin: { symbol: "", name: "0x035e...2a7b39" },
+
+    quantity: "$130,000",
+    percentage: 6.998667555,
+  },
+  {
+    id: 4,
+    coin: { symbol: "", name: "0x2ee9...ce8363" },
+    quantity: "$117.000",
+    percentage: 6.665556296,
+  },
+  {
+    id: 5,
+    coin: { symbol: "", name: "0x406b...93ae0f" },
+    quantity: "$68,000",
+    percentage: 5.46568954,
+  },
+  {
+    id: 6,
+    coin: { symbol: "", name: "0x2bf8...bbd8e0" },
+    quantity: "$55,000",
+    percentage: 4.965822785,
+  },
+  {
+    id: 7,
+    coin: { symbol: "", name: "0x0000...000000" },
+    quantity: "$46,000",
+    percentage: 4.199200533,
+  },
+  {
+    id: 8,
+    coin: { symbol: "", name: "0x227e...0ccc64" },
+    quantity: "$42,500",
+    percentage: 3.999333777,
+  },
+  {
+    id: 9,
+    coin: { symbol: "", name: "0xdd47...106ac2" },
+    quantity: "$42,000",
+    percentage: 3.866089274,
+  },
+  {
+    id: 10,
+    coin: { symbol: "", name: "0x71e6...300a67" },
+    quantity: "$21,900",
+    percentage: 1.799467022,
+  },
+];
