84dfdfaabd73cb4563fabb90d7a1bc29fef0aa7b
Smaran Ummadishetty <speak2smaran@gmail.com>
2022-09-15 22:32:12 +0530

    update data js

--- src/data1.js
+++ src/data1.js
@@ -2896,8 +2896,8 @@ export default [
   {
     HolderAddress: "0xde737130a939f2B364834a3C6b721f72f5718e72",
     Capital: "$7,000",
-    Quantity: "$14,700",
+    Quantity: "$10,700",
     profit: "110%",
-    percentage: "1.066622252%",
+    percentage: "1.00002252%",
   },
 ];
